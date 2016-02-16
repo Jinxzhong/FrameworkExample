0fac41f479162f35ec23ba118499a61b61e5db48
LinQinan <742890434@qq.com>
2016-02-16 23:57:53 +0800

    commit Gson

--- app/src/main/java/com/example/allen/frameworkexample/volley/MainActivity.java
+++ app/src/main/java/com/example/allen/frameworkexample/volley/MainActivity.java
@@ -8,16 +8,13 @@ import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.allen.frameworkexample.R;
-
-import org.json.JSONObject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = "MainActivity";
     @Bind(R.id.textview)
     TextView mTextview;
     @Bind(R.id.imageview)
@@ -33,28 +30,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void getJson() {
-        //新建一个请求
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://www.mocky.io/v2/56c30376120000be2e73f210", null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("mTAG", response.toString());
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (App.getInstance() != null) {
+            App.getInstance().cancelPendingRequests(TAG);
+        }
+    }
 
+    private void getJson() {
+        //新建一个JsonObject请求
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://www.mocky.io/v2/56c33f991200002d3773f261", null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.d(TAG, response.toString());
+//                        mTextview.setText(response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e(TAG, error.getMessage(), error);
+//            }
+//        });
+        //新建一个JsonObject请求
+        GsonRequest<Person> gsonRequest = new GsonRequest<Person>(
+                "http://www.mocky.io/v2/56c33f991200002d3773f261", Person.class,
+                new Response.Listener<Person>() {
+                    @Override
+                    public void onResponse(Person person) {
+                        Log.d(TAG, "first_name: " + person.getFirst_name());
+                        Log.d(TAG, "last_name: " + person.getLast_name());
+                        Log.d(TAG, "gender: " + person.getGender());
+                        mTextview.setText("first_name: " + person.getFirst_name() + "\n"
+                                + "last_name: " + person.getLast_name() + "\n" +
+                                "gender: " + person.getGender());
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("mTAG", error.getMessage(), error);
+                Log.e(TAG, error.getMessage(), error);
             }
         });
+
         //添加请求到队列
-        App.getInstance().addToRequestQueue(jsonObjectRequest);
+        App.getInstance().addToRequestQueue(gsonRequest, TAG);
     }
 
     private void getImage() {
 
     }
-
-
 }
